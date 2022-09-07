f69d49e07147561050e188ae8760b421c65872c6
voryzhen <you@example.com>
2022-09-07 12:05:07 +0700

    Implemented REPL

--- src/main.c
+++ src/main.c
@@ -1,7 +1,67 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main ()
+typedef struct
 {
-    printf ( "Hello\n" );
-    return 0;
+  char*   buffer;
+  size_t  buffer_length;
+  ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* create_input_buffer()
+{
+    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+
+    input_buffer->buffer        = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length  = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() { printf("db > "); }
+
+void read_input(InputBuffer* input_buffer)
+{
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0)
+    {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input_buffer->input_length = bytes_read - 1; // new line symbol
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void destroy_input_buffer(InputBuffer* input_buffer)
+{
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+int main(int argc, char* argv[])
+{
+    InputBuffer* input_buffer = create_input_buffer();
+
+    bool is_operating = true;
+
+    while (is_operating)
+    {
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        {
+            destroy_input_buffer(input_buffer);
+            is_operating = false;
+        }
+        else
+        {
+            printf("Undefined command '%s'.\n", input_buffer->buffer);
+        }
+    }
 }
