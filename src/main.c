09679a8620fb4aa5e00c00b5cb23a8ea54bd9e5b
voryzhen <you@example.com>
2022-09-07 12:10:29 +0700

    refactor

--- src/main.c
+++ src/main.c
@@ -10,9 +10,9 @@ typedef struct
   ssize_t input_length;
 } InputBuffer;
 
-InputBuffer* create_input_buffer()
+InputBuffer* create_input_buffer ()
 {
-    InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
+    InputBuffer* input_buffer = (InputBuffer*) malloc (sizeof(InputBuffer));
 
     input_buffer->buffer        = NULL;
     input_buffer->buffer_length = 0;
@@ -21,11 +21,11 @@ InputBuffer* create_input_buffer()
     return input_buffer;
 }
 
-void print_prompt() { printf("db > "); }
+void print_prompt () { printf("db > "); }
 
-void read_input(InputBuffer* input_buffer)
+void read_input (InputBuffer* input_buffer)
 {
-    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    ssize_t bytes_read = getline (&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
     if (bytes_read <= 0)
     {
@@ -37,31 +37,37 @@ void read_input(InputBuffer* input_buffer)
     input_buffer->buffer[bytes_read - 1] = 0;
 }
 
-void destroy_input_buffer(InputBuffer* input_buffer)
+void destroy_input_buffer (InputBuffer* input_buffer)
 {
-    free(input_buffer->buffer);
-    free(input_buffer);
+    free (input_buffer->buffer);
+    free (input_buffer);
 }
 
-int main(int argc, char* argv[])
+bool evaluate (InputBuffer* input_buffer)
 {
-    InputBuffer* input_buffer = create_input_buffer();
+    if (strcmp (input_buffer->buffer, ".exit") == 0)
+    {
+        return false;
+    }
+    else
+    {
+        printf ("Undefined command '%s'.\n", input_buffer->buffer);
+        return true;
+    }
+}
+
+int main (int argc, char* argv[])
+{
+    InputBuffer* input_buffer = create_input_buffer ();
 
     bool is_operating = true;
 
     while (is_operating)
     {
-        print_prompt();
-        read_input(input_buffer);
-
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
-        {
-            destroy_input_buffer(input_buffer);
-            is_operating = false;
-        }
-        else
-        {
-            printf("Undefined command '%s'.\n", input_buffer->buffer);
-        }
+        print_prompt ();
+        read_input (input_buffer);
+        is_operating = evaluate (input_buffer);
     }
+
+    destroy_input_buffer (input_buffer);
 }
