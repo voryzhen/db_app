15624e317635ad351f0b4a1363935465594a65e4
voryzhen <you@example.com>
2022-09-08 16:32:19 +0700

    Move code from meta and sql cmds

--- src/main.c
+++ src/main.c
@@ -4,6 +4,8 @@
 #include <string.h>
 
 #include "input_buffer.h"
+#include "meta_cmd.h"
+#include "sql_cmd.h"
 
 void print_prompt () { printf("db > "); }
 
@@ -13,86 +15,13 @@ void read_input (InputBuffer* input_buffer)
 
     if (bytes_read <= 0)
     {
-        printf("Error reading input\n");
-        exit(EXIT_FAILURE);
+        printf ("Error reading input\n");
+        destroy_input_buffer (input_buffer);
+        exit (EXIT_FAILURE);
     }
 
     input_buffer->input_length = bytes_read - 1; // new line symbol
     input_buffer->buffer[bytes_read - 1] = 0;
-}
-
-
-typedef enum {
-    META_CMD_SUCCESS,
-    META_CMD_UNDEFINED_CMD
-} MetaCmdResult;
-
-typedef enum { PARCE_SUCCESS, PARCE_UNDEFINED_CMD } ParceSqlCmdResult;
-
-MetaCmdResult execute_meta_cmd (InputBuffer* input_buffer)
-{
-    if (strcmp(input_buffer->buffer, ".exit") == 0)
-    {
-        exit(EXIT_SUCCESS);
-    }
-    else
-    {
-        return META_CMD_UNDEFINED_CMD;
-    }
-}
-
-typedef enum
-{
-    SQL_CMD_INSERT,
-    SQL_CMD_SELECT
-} SqlCmdType;
-
-typedef struct
-{
-  SqlCmdType type;
-} SqlCmd;
-
-ParceSqlCmdResult parce_sql_cmd (InputBuffer* input_buffer, SqlCmd* cmd)
-{
-    if (strncmp (input_buffer->buffer, "insert", 6) == 0)
-    {
-        cmd->type = SQL_CMD_INSERT;
-        return PARCE_SUCCESS;
-    }
-    if (strcmp (input_buffer->buffer, "select") == 0)
-    {
-        cmd->type = SQL_CMD_SELECT;
-        return PARCE_SUCCESS;
-    }
-
-    return PARCE_UNDEFINED_CMD;
-}
-
-void execute_sql_cmd (SqlCmd* cmd)
-{
-    switch (cmd->type)
-    {
-        case (SQL_CMD_INSERT):
-            printf("Executing insert.\n");
-            break;
-        case (SQL_CMD_SELECT):
-            printf("Executing select.\n");
-            break;
-    }
-}
-
-void process_sql_cmd (InputBuffer* input_buffer)
-{
-    SqlCmd cmd;
-    switch ( parce_sql_cmd (input_buffer, &cmd) ) {
-        case PARCE_SUCCESS:
-            execute_sql_cmd (&cmd);
-            printf ("Executed.\n");
-            break;
-        case PARCE_UNDEFINED_CMD:
-            printf ("Undefined sql command '%s'.\n", input_buffer->buffer);
-            break;
-    }
 }
 
 void process_cmd (InputBuffer* input_buffer)
@@ -111,13 +40,10 @@ void process_cmd (InputBuffer* input_buffer)
 int main (int argc, char* argv[])
 {
     InputBuffer* input_buffer = create_input_buffer ();
-
     while (true)
     {
         print_prompt ();
         read_input   (input_buffer);
         process_cmd  (input_buffer);
     }
-
-    destroy_input_buffer (input_buffer);
 }
---
+++ src/meta_cmd.c
@@ -0,0 +1,16 @@
+#include "meta_cmd.h"
+
+#include <string.h>
+
+MetaCmdResult execute_meta_cmd (InputBuffer* input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        destroy_input_buffer (input_buffer);
+        exit (EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_CMD_UNDEFINED_CMD;
+    }
+}
